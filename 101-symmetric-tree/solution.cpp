ed013fb84e2a1c838ecb821deb99b3347661b884
tarek-gritli <tarek.gritli@insat.ucar.tn>
2024-05-14 12:43:54 +0100

    Add c++ solution to 101-symmetric-tree, Time: O(n), Space: O(n)

---
+++ 101-symmetric-tree/solution.cpp
@@ -0,0 +1,28 @@
+// Runtime: 3ms, Beats 70.22% of users with Python3
+// Memory: 17.39MB, Beats 37.62% of users with Python3
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSymmetric(TreeNode* root) {
+        if (root == nullptr) {
+            return true;
+        }
+        return compare(root->left,root->right);
+    }
+    bool compare(TreeNode* n1, TreeNode* n2) {
+        if (n1 == nullptr && n2 == nullptr) return true;
+        if (n1==nullptr || n2==nullptr) return false;
+        return n1->val == n2->val && compare(n1->right,n2->left) && compare(n1->left,n2->right);
+    }
+};
