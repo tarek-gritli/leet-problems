d77e1c1c350fbd4f4ae6b4d69d9d4553cf1a2474
tarek-gritli <tarek.gritli@insat.ucar.tn>
2024-09-08 22:42:33 +0100

    Add java solution to 1971-find-if-path-exists-in-graph using weighted quick union

---
+++ 1971-find-if-path-exists-in-graph/solution.java
@@ -0,0 +1,49 @@
+class QuickUnion {
+    private int[] id;
+    private int[] sz;
+
+    public QuickUnion(int N) {
+        id = new int[N];
+        sz = new int[N];
+        for (int i = 0; i < N; i++) {
+            id[i] = i;
+            sz[i] = 1;
+        }
+    }
+
+    public int root(int i) {
+        while (i != id[i])
+            i = id[i];
+        return i;
+    }
+
+    public boolean connected(int p, int q) {
+        return root(p) == root(q);
+    }
+
+    public void union(int p, int q) {
+        int rootp = root(p);
+        int rootq = root(q);
+        if (rootp == rootq)
+            return;
+
+        if (sz[rootp] < sz[rootq]) {
+            id[rootp] = rootq;
+            sz[rootq] += sz[rootp];
+        } else {
+            id[rootq] = rootp;
+            sz[rootp] += sz[rootq];
+        }
+    }
+
+}
+
+class Solution {
+    public boolean validPath(int n, int[][] edges, int source, int destination) {
+        QuickUnion uf = new QuickUnion(n);
+        for (int[] edgs : edges) {
+            uf.union(edgs[0], edgs[1]);
+        }
+        return uf.connected(source, destination);
+    }
+}
